16c4130379cd1b48b703788132e2f32b60879861
SeorangDion <92658890+SeorangDion@users.noreply.github.com>
2022-01-18 15:38:46 +0700

    Update __main__.py

--- PrimeMega/__main__.py
+++ PrimeMega/__main__.py
@@ -784,7 +784,7 @@ def main():
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
             dispatcher.bot.sendMessage(
-                f@{SUPPORT_CHAT}", 
+                f"@{SUPPORT_CHAT}", 
                 "👋 Hi friends, I'm alive.",
                 parse_mode=ParseMode.MARKDOWN
             )
