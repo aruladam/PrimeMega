7e9792011777cad7b22a4240f0312a6d731c3a16
SeorangDion <92658890+SeorangDion@users.noreply.github.com>
2022-01-18 13:06:07 +0700

    Update alive.py

--- PrimeMega/modules/alive.py
+++ PrimeMega/modules/alive.py
@@ -9,16 +9,16 @@ from PrimeMega.events import register
 from PrimeMega import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/efc27dec817626cc95016.jpg"
+PHOTO = "https://telegra.ph/file/3898f51ac3e9b4b098283.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Prime Mega.** \n\n"
+  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Dion Bot.** \n\n"
   PRIME += "⚪ **I'm Working Properly** \n\n"
-  PRIME += f"⚪ **My Master : [Lord](https://t.me/Bukan_guudlooking)** \n\n"
+  PRIME += f"⚪ **My Master : [Dion](https://t.me/seorangdion)** \n\n"
   PRIME += f"⚪ **Library Version :** `{telever}` \n\n"
   PRIME += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   PRIME += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   PRIME += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/PrimeMegaBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/PrimeSupportGroup")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/DionXRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/DionXSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PRIME,  buttons=BUTTON)
