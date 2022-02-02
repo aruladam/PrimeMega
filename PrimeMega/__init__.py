c43429844b69c4a8428afaa69a030ae57f8f82c3
aruladam <95761288+aruladam@users.noreply.github.com>
2022-02-02 13:43:46 +0000

    Update __init__.py

--- PrimeMega/__init__.py
+++ PrimeMega/__init__.py
@@ -40,7 +40,7 @@ logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLeve
 LOGGER = logging.getLogger('[PrimeMega]')
 LOGGER.info("Prime is starting. | An Prime Mega Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/Tonic990 (t.me/Bukan_guudlooking)")
+LOGGER.info("Project maintained by: github.com/Tonic990 (t.me/skyiarul)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 9:
