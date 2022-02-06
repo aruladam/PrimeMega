f74d40f2b321a514f974268524b80809fd699655
Dion <92658890+SeorangDion@users.noreply.github.com>
2022-02-06 12:25:42 +0700

    Update forcesubs.py

--- PrimeMega/modules/forcesubs.py
+++ PrimeMega/modules/forcesubs.py
@@ -109,7 +109,7 @@ def _check_member(client, message):
                     )
                 except ChatAdminRequired:
                     sent_message.edit(
-                        "😕 **Adam Bot is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
+                        "😕 **Mordred is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
                     )
 
             except ChatAdminRequired:
@@ -179,7 +179,7 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❂ adam Bot can mute members who are not subscribed your channel until they subscribe
+❂ Mordred can mute members who are not subscribed your channel until they subscribe
 ❂ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 ❂*Setup*
 *Only creator*
