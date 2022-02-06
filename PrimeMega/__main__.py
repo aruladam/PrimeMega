f4fff200681e174912ae487036c9903fc2c3aeed
aruladam <95761288+aruladam@users.noreply.github.com>
2022-02-06 10:04:53 +0000

    Update __main__.py

--- PrimeMega/__main__.py
+++ PrimeMega/__main__.py
@@ -81,7 +81,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
- ───「[Adam Robot](https://t.me/AdamXRobot)」───
+ ───「[Mordred Robot](https://t.me/MordredRobot)」───
 *Hello {} !*
 ────────────────────────
 Adam Robot a powerful group management bot built to help you manage your group!
